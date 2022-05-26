90791f8749279615b91984b99c7ea02eb4324df1
Gema Martin <geminway@live.com>
2022-05-26 11:54:20 +0200

    - Fixed bug that made the images in the patch/chart look deformed because they didn't have a width. - Added alt in the img

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 interface CardsLanding {
   srcImg: string,
-  nameText: string
+  nameText: string,
+  alt: string
 }
 
 @Component({
@@ -13,8 +14,8 @@ interface CardsLanding {
 export class HomeComponent implements OnInit {
 
   cardsLanding: CardsLanding[] = [
-    { srcImg: '../../../assets/img/Pizza-on-wooden-board.jpg', nameText: '¿Aún no has probado nuestras pizzas? ¡Pruébalas!'},
-    { srcImg: '../../../assets/img/deliveryMan.jpg', nameText: '¡Pide para llevar!'},
+    { srcImg: '../../../assets/img/Pizza-on-wooden-board.jpg', nameText: '¿Aún no has probado nuestras pizzas? ¡Pruébalas!', alt: 'Link carta'},
+    { srcImg: '../../../assets/img/deliveryMan.jpg', nameText: '¡Pide para llevar!', alt: 'Link pedir pizza'},
   ]
   constructor() { }
 
